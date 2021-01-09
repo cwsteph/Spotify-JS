e686071167cc08cf033d23011b0b3ae6fb9c8cbd
Charles Stephens <charles.stephens.w@gmail.com>
2021-01-09 11:20:01 -0700

    fixed some issues

--- spotify-playlist.js
+++ spotify-playlist.js
@@ -1,21 +1,21 @@
 var request = require('request');
 var user_id = "1221063701";
-var token = "Bearer BQApN10GDLRVmeQ_Ozvh9ySo32dRoSvBC6bUYIzHLD5IgxxgimJZrqwg-b0u65Aiktx1Xt3kqg7Nb5fcVWy31bha1NGON7coQeGMV-XwEnerTW0lPC7uowHSm2IUDZ_4TBYhKm-hEjSRt_G0xw"
+var token = "Bearer BQCk-LdPiCf_MAilX2I-q28xRNqPE3j6xild3YWYTyGJ4MQwLgonIr8ccxpmeDNITCmKSHj9zhklMW4_QPlzttX1Is2p1v57ASjQTh0CGJKevmdIg1KY4hlZnyNeu98MCoFXnL3aijldLUwXCA"
 var playlists_url = "https://api.spotify.com/v1/users/"+user_id+"/playlists";
 
-request({url:playlists_url, headers:{"Authorization":token}}, function(err,res){
-    if (res) {
-        var playlists = JSON.parse(res.body);
-        console.log(JSON.stringify(playlists.items, null, " "));
-        var playlist_url = playlists.items[0].href;
-        request({url:playlist_url, headers:{"Authorization":token}}, function(err,res){
-            if (res) {
-                var playlist = JSON.parse(res.body);
-                console.log("playlist: " + playlist.name);
-                playlist.tracks.items.forEach(function(track){
-                    console.log(track.track.name);
-                });
-            }
-        })
-    }
+request({url:playlists_url, headers:{"Authorization":token}}, function(err, res){
+	if (res){
+		var playlists=JSON.parse(res.body);	
+		var playlist_url = playlists.items[0].href;
+		request({url:playlist_url, headers:{"Authorization":token}}, function(err, res){
+			if (res){
+				var playlist = JSON.parse(res.body);
+				console.log("playlist: " + playlist.name);
+				playlist.tracks.items.forEach(function(track){
+					console.log(track.track.name);
+				});
+			}
+		})		
+	}
 })
+
