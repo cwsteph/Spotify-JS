0bc33625c12583124bd0bc898a44983c1f5b6280
Charles Stephens <charles.stephens.w@gmail.com>
2021-01-11 20:27:23 -0700

    added jupyter

--- spotify-playlist.js
+++ spotify-playlist.js
@@ -1,6 +1,6 @@
 var request = require('request');
 var user_id = "1221063701";
-var token = "Bearer BQCk-LdPiCf_MAilX2I-q28xRNqPE3j6xild3YWYTyGJ4MQwLgonIr8ccxpmeDNITCmKSHj9zhklMW4_QPlzttX1Is2p1v57ASjQTh0CGJKevmdIg1KY4hlZnyNeu98MCoFXnL3aijldLUwXCA"
+var token = "Bearer BQDyMmKkkKZ2vJrpz2jvKaKpA3gJFCCzW-TLOna3ykWJAGycFr3pCtAJ3P2RWcRZTDTo7GooINu6OJhneaoGMaO5jiF2pEUY3vj1EuMjSfFHRWJ7MLt1gPzNNl3eIoc0JuiWRaVKKIPWj2IYNQ"
 var playlists_url = "https://api.spotify.com/v1/users/"+user_id+"/playlists";
 
 request({url:playlists_url, headers:{"Authorization":token}}, function(err, res){
